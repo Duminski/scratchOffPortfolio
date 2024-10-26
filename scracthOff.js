6156e3b3a2032bfcbf650fc4d254c5da9f736fea
Duminski <40200777+Duminski@users.noreply.github.com>
2024-10-26 14:18:31 +0200

    Update scracthOff.js

--- scracthOff.js
+++ scracthOff.js
@@ -1,7 +1,7 @@
 $(document).ready((function() {
 
 var image = { // back and front images
-  'back': { 'url':'./cv.jpg', 'img':null },
+  'back': { 'url':'./scratch2.jpg', 'img':null },
 	'front': { 'url':'./scratch.jpg', 'img':null }
 };
 
